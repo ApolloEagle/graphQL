53a78689aba8b978aebcce06e137d4e8da73b9ed
ApolloEagle <sobeksea@gmail.com>
2022-01-12 12:34:46 -0700

    shell application

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const { ApolloServer, gql } = require("apollo-server-express");
+
+const typeDefs = null;
+const resolvers = null;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.listen(3000, () =>
+  console.info("Apollo GraphQL server is running on port 3000")
+);
