8bd69d19a4a9aa9988b30678a2115501b1ca82c3
ApolloEagle <sobeksea@gmail.com>
2022-01-12 12:51:04 -0700

    type defs and resolvers

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const app = express();
 const { ApolloServer, gql } = require("apollo-server-express");
+const { start } = require("repl");
 
-const typeDefs = null;
-const resolvers = null;
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+  type User {
+    name: String!
+  }
+`;
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        name: "Ferguson",
+      };
+    },
+  },
+};
 
-server.applyMiddleware({ app });
+const startApolloServer = async (typeDefs, resolvers) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
 
-app.listen(3000, () =>
-  console.info("Apollo GraphQL server is running on port 3000")
-);
+  app.listen(3000, () =>
+    console.info("Apollo GraphQL server is running on port 3000")
+  );
+};
+
+startApolloServer(typeDefs, resolvers);
