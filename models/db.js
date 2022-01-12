37f4d1b20fb01a72f2d6db21e0b3ee7204914c39
ApolloEagle <sobeksea@gmail.com>
2022-01-12 15:26:23 -0700

    init sequelize

--- models/db.js
+++ models/db.js
@@ -1,2 +1,12 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("graphql", "root", "", {});
+const sequelize = new Sequelize("graphql", "root", "", {
+  dialect: "mysql",
+  operatorAliases: false,
+  define: {
+    timestamps: false,
+  },
+});
+
+module.exports = {
+  sequelize,
+};
