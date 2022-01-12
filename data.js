94c2c2f9d4a7df4c3ed7ae0cb24d513973e36830
ApolloEagle <sobeksea@gmail.com>
2022-01-12 12:59:25 -0700

    static dataset

---
+++ data.js
@@ -0,0 +1,18 @@
+let users = [
+  {
+    id: 0,
+    name: "Jimmy",
+  },
+  {
+    id: 1,
+    name: "Ferguson",
+  },
+  {
+    id: 2,
+    name: "Finnigan",
+  },
+];
+
+module.exports = {
+  users,
+};
